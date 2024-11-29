5044aa3f01bb0a9c7cf37db5a33e62637b6dff64
Sven Harazim <sven.harazim@landrix.de>
2024-11-29 17:40:31 +0100

    mdb sidebar example

---
+++ 01_MDB-Sidebar/tms/Unit1.pas
@@ -0,0 +1,24 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
+  WEBLib.Forms, WEBLib.Dialogs;
+
+type
+  TForm1 = class(TWebForm)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
